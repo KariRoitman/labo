d80151a18a88ddc2d0107da0652bf15c822c5369
Karina Roitman <karina.roitman2@gmail.com>
2023-04-20 08:03:13 -0300

    MODIFICACION DE SEMILLA Y PARAMETROS

--- src/lightgbm/z424_lightgbm_final.r
+++ src/lightgbm/z424_lightgbm_final.r
@@ -20,16 +20,17 @@ PARAM$input$dataset       <- "./datasets/dataset_pequeno.csv"
 PARAM$input$training      <- c( 202107 )   #meses donde se entrena el modelo
 PARAM$input$future        <- c( 202109 )   #meses donde se aplica el modelo
 
-PARAM$finalmodel$semilla           <- 102191
+PARAM$finalmodel$semilla           <- 201107
 
-PARAM$finalmodel$num_iterations    <-  559
-PARAM$finalmodel$learning_rate     <-    0.0100746999
-PARAM$finalmodel$feature_fraction  <-    0.5144127527
-PARAM$finalmodel$min_data_in_leaf  <-    505
-PARAM$finalmodel$num_leaves        <-     44
+PARAM$finalmodel$num_iterations    <-  31
+PARAM$finalmodel$learning_rate     <-   0.0111411453754121
+PARAM$finalmodel$feature_fraction  <-   0.999711460397934
+
+PARAM$finalmodel$min_data_in_leaf  <-    639
+PARAM$finalmodel$num_leaves        <-     1024
 
 
-PARAM$finalmodel$max_bin           <-     31
+PARAM$finalmodel$max_bin           <-     0
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
