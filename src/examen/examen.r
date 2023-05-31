650e7918c3cee729c592be93f505e406a0afbfa1
Karina Roitman <karina.roitman2@gmail.com>
2023-05-31 19:29:03 -0300

    examen

--- src/examen/examen.r
+++ src/examen/examen.r
@@ -10,7 +10,7 @@ gc()             #garbage collection
 require("data.table")
 
 #Parametros del script
-kexp_input  <- "HT8510"   #Aqui debe ir SU  bayesiana elegida
+kexp_input  <- "09c_HT_60Var2"   #Aqui debe ir SU  bayesiana elegida
 # FIN Parametros del script
 
 #------------------------------------------------------------------------------
